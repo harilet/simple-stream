1a484477fa81d02ce0fe5422d7b66be863c1cc74
harilet <40688601+harilet@users.noreply.github.com>
2020-09-02 14:23:39 +0530

    Update server_V02.py

--- server_V02.py
+++ server_V02.py
@@ -3,37 +3,24 @@ from os import listdir, getcwd, chdir, remove, chmod
 from os.path import isfile
 from shutil import copy
 
-option = ["Movie", "anime"]
-global folder
-folder = ""
-tree = []
-# chdir(folder)
-stat = "E:\\stream\\static"
+
+global folder,option,tree,stat
 app = Flask(__name__)
 
-port = 5000
-ip = "192.168.1.10"
 
-
-@app.route('/browse')
+@app.route('/')
 def browse():
     files = listdir(getcwd())
     t = [files[i:i + 3] for i in range(0, len(files), 3)]
     return render_template('page_temp.html', items=t, port=port, ip=ip)
 
 
-@app.route('/')
-def index():
-    t = [option[i:i + 3] for i in range(0, len(option), 3)]
-    return render_template('main.html', items=t, port=port, ip=ip)
-
-
-@app.route('/browse/<file>')
+@app.route('/<file>')
 def downloadFile(file):
     global folder
     if isfile(file):
 
-        if file[-4:] == ".mp4" or file[-4:] == ".mkv" or file[-4:]=='.avi':
+        if file[-4:] == ".mp4":
             r_files = listdir(stat)
             for i in r_files:
                 chmod(stat + "\\" + i, 0o777)
@@ -56,16 +43,13 @@ def back():
     return redirect(url_for('browse'))
 
 
-@app.route('/<choice>')
-def select(choice):
-    global folder
-    if choice == "Movie":
-        folder = "E:\\Movie"
-    elif choice == "anime":
-        folder = "F:\\anime"
-    chdir(folder)
-    return redirect(url_for('browse'))
-
-
 if __name__ == '__main__':
-    app.run(host=ip, port=port, debug=True)
+
+    folder =str(input("Enter movie folder location:"))
+    tree = []
+    chdir(folder)
+    stat = str(input("Enter static folder location:"))
+    port = 5000
+    ip = str(input("Enter IP address:"))
+    app.run(host=ip, port=port, debug=True, use_reloader=False)
+
